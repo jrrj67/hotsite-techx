778a3493d6d2953b003e5c2b25420063c5aaa3bd
Ricardo <ricardojunior.6767@gmail.com>
2020-02-12 16:10:53 -0300

    add https

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="public/css/materialize.css">
+        <link rel="stylesheet" href="{{asset("css/materialize.css")}}">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <title>FiscalHelper</title>
     </head>
@@ -166,7 +166,7 @@
         src="https://code.jquery.com/jquery-2.2.4.min.js"
         integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
         crossorigin="anonymous"></script>
-    <script src="/public/js/materialize.js"></script>
+    <script src="{{asset("js/materialize.js")}}"></script>
 @endsection
 </body>
 </html>
