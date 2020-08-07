5fb1d79fc74bb2067a3e9bfec12bd4f758e10d74
José Ricardo <ricardojunior.6767@gmail.com>
2020-08-07 14:38:40 -0300

    Icon

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -6,8 +6,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="mobile-web-app-capable" content="yes">
-        <link rel="shortcut icon" sizes="192x192" href="{{asset('images/logo-techx.png')}}">
+        <link rel="apple-touch-icon" sizes="128x128" href="{{asset('images/logo-techx.png')}}">
         <link rel="stylesheet" href="{{asset("css/materialize.css")}}">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <title>Ajuda TechX</title>
