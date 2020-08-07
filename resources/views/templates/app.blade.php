00934a036cca1882d465a858b3ee497011ab2ea2
José Ricardo <ricardojunior.6767@gmail.com>
2020-08-07 14:52:44 -0300

    Icon

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -6,7 +6,9 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="{{asset('images/favicon.ico')}}" rel="shortcut icon" type="image/x-icon">
         <link rel="apple-touch-icon" type="image/x-icon" sizes="64x64" href="{{asset('images/favicon.ico')}}">
+        <link rel="icon" type="image/png" sizes="64x64" href="{{asset('images/logo-techx.png')}}">
         <link rel="stylesheet" href="{{asset("css/materialize.css")}}">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <title>Ajuda TechX</title>
