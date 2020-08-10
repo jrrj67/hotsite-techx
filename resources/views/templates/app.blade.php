aafaec5c4c1f9b20af9d7c1f561be347fb03620b
José Ricardo <ricardojunior.6767@gmail.com>
2020-08-10 13:54:56 -0300

    Trocando ícones

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -6,7 +6,15 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-{{--        <link rel="shortcut icon" href="{{ asset('c.ico') }}" type="image/x-icon"/>--}}
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset("images/favicons/apple-touch-icon.png")}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset("images/favicons/favicon-32x32.png")}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset("images/favicons/favicon-16x16.png")}}">
+        <link rel="manifest" href="{{asset("images/favicons/site.webmanifest")}}">
+        <link rel="mask-icon" href="{{asset("images/favicons/safari-pinned-tab.svg")}}" color="#5bbad5">
+        <link rel="shortcut icon" href="public/images/favicons/favicon.ico">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="{{asset("images/favicons/browserconfig.xml")}}">
+        <meta name="theme-color" content="#fa931a">
         <link rel="stylesheet" href="{{asset("css/materialize.css")}}">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <title>Ajuda TechX</title>
